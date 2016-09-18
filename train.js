e10acda8cc76f94051ad689778f8f7cc7b9d0474
Kevin Shukla <kevn1841@gmail.com>
2016-09-18 15:19:26 -0400

    cleaned up code

--- train.js
+++ train.js
@@ -35,8 +35,6 @@
 
 trainData.ref().on("child_added", function(childSnapshot, prevChildKey){
 
-console.log(childSnapshot.val());
-
 var tName = childSnapshot.val().name;
 var tDestination = childSnapshot.val().destination;
 var tFrequency = childSnapshot.val().frequency;
@@ -48,12 +46,6 @@ var tRemainder = moment().diff(moment.unix(tFirstTrain), "minutes") % tFrequency
 var tMinutes = tFrequency - tRemainder;
 
 var tArrival = moment().add(tMinutes, "m").format("hh:mm A"); 
-console.log(tMinutes);
-console.log(tArrival);
-
-console.log(moment().format("hh:mm A"));
-console.log(tArrival);
-console.log(moment().format("X"));
 
 $("#tData").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tFrequency + "</td><td>" + tArrival + "</td><td>" + tMinutes + "</td></tr>");
 
